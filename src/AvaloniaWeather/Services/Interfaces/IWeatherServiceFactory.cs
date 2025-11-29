cdc2d8f3c15e89314b209db4c98eb544c768c74b
Sebastian Jensen <apps@tsjdev-apps.de>
2025-11-29 22:27:13 +0100

    Add WeatherServiceFactory and interface
    Introduces IWeatherServiceFactory interface and its implementation, WeatherServiceFactory, to encapsulate the creation of IWeatherService instances using an API key. This supports dependency injection and improves testability.

---
+++ src/AvaloniaWeather/Services/Interfaces/IWeatherServiceFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace AvaloniaWeather.Services.Interfaces;
+
+/// <summary>
+/// Factory interface for creating IWeatherService instances.
+/// </summary>
+public interface IWeatherServiceFactory
+{
+    /// <summary>
+    /// Creates an instance of IWeatherService with the provided API key.
+    /// </summary>
+    /// <param name="apiKey">OpenWeatherMap API key for authentication</param>
+    /// <returns>Configured IWeatherService instance</returns>
+    IWeatherService CreateWeatherService(string apiKey);
+}
---
+++ src/AvaloniaWeather/Services/WeatherServiceFactory.cs
@@ -0,0 +1,13 @@
+﻿using AvaloniaWeather.Services.Interfaces;
+
+namespace AvaloniaWeather.Services;
+
+/// <summary>
+/// Factory for creating IWeatherService instances.
+/// </summary>
+public class WeatherServiceFactory : IWeatherServiceFactory
+{
+    /// <inheritdoc/>
+    public IWeatherService CreateWeatherService(string apiKey)
+        => new WeatherService(apiKey);
+}
