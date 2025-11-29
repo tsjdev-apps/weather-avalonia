fbaef6eff0086489037819c4420218200accfd83
Sebastian Jensen <apps@tsjdev-apps.de>
2025-11-29 20:59:45 +0100

    Add value converters for UI data formatting
    Introduced four new converters: AqiToBrushConverter for mapping AQI values to colors, MetersToKmConverter for formatting meters as kilometers, NullToVisibilityConverter for null-to-visibility logic, and UnitSuffixConverter for appending unit suffixes to numeric values. These converters support improved data binding and display in the AvaloniaWeather application.

---
+++ src/AvaloniaWeather/Converters/MetersToKmConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace AvaloniaWeather.Converters;
+
+/// <summary>
+/// Converts a distance in meters to a formatted string in kilometers.
+/// </summary>
+public class MetersToKmConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value is double meters ? $"{meters / 1000:F1-} km" : "-";
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
---
+++ src/AvaloniaWeather/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace AvaloniaWeather.Converters;
+
+/// <summary>
+/// Converts a null value to a boolean indicating visibility.
+/// </summary>
+public class NullToVisibilityConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value != null;
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
